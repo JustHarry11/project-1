b92fa0af5417bda8d6c5e76aa2cd4a11ee39289a
Harry <harrylippa@gmail.com>
2025-03-28 12:42:26 +0000

    Project One

---
+++ scripts/app.js
@@ -0,0 +1,45 @@
+// ! Recursion
+
+// I will use recursion once the button is click and the slot is empty, it will check and reveil all
+// surrounding slots and stops when it finds one with a number ( which shows a mine is nearby)
+
+/*function recursiveMineSweep(){
+    if(slot is next to a named slot){
+        // stop recursion
+    }
+    recurse();
+}*/
+
+// ! Psedocode
+
+// * When the game starts up I want to see a welcome message and a button which begins the game.
+//   Also a pannel on the left which gives the player instructions on how to play.
+
+// * Once the player begins the game, a 10x10 grid will appear in the center of the screen
+//   Above the grid is a timer to show how long the game has lasted
+//   On the other side is the amount of flags you have remaining
+
+// ? How will I generate the bombs onto the grid and the numbers displaying how many bombs are around each tile
+//   The will start with a function which spawnsBombs
+//      - It will spawn 10 random bombs across the grid
+// !        - Give each grid slot a number and generate 10 random numbers using math.floor and math.random
+//          - Assinged the grid spot with the corresponding number a class of bomb but keep it hidden 
+//      - Once spawned each individual grid slot will check how many bombs are around them
+//          - This is achived using the forEach function
+//          - Checking each slot and whether the each a bomb on the surrounding grid and log the amout
+//          - It will display the about of bombs
+
+// * Once the game has begun the player can choose which grid slot to select
+//   This will begin the timer
+//   Once the slot is select it will either show
+//      - An empty show an empty slow which will begin a flood fill and check all the connecting empty slots
+//      - A number of the amout of bombs which are surounding it
+//      - A bomb which will end the game
+//          - A message will apear and the player will be able to play again
+// ? How will the player put flag on the slots they think have bombs inside
+//   The player will right click the slot and a class will be added which displays a image of a flag to notify the player.
+
+// * The player will keep on going until he has completed the board and what is left are the slots which have the bombs inside
+//      - A message will appear congratulating them on winning the game
+//      - The time take will appear underneath
+//      - They will click a button which allows them to play again
