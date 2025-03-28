fa88bc71db6b5401777f06bd18321e66372c1aa6
Harry <harrylippa@gmail.com>
2025-03-28 15:26:02 +0000

    Project One

--- scripts/app.js
+++ scripts/app.js
@@ -22,24 +22,28 @@
 // ? How will I generate the bombs onto the grid and the numbers displaying how many bombs are around each tile
 //   The will start with a function which spawnsBombs
 //      - It will spawn 10 random bombs across the grid
-// !        - Give each grid slot a number and generate 10 random numbers using math.floor and math.random
+// !          I can image having some trouble with assigning the slot a class
+//          - Generate 10 random numbers using math.floor and math.random
 //          - Assinged the grid spot with the corresponding number a class of bomb but keep it hidden 
 //      - Once spawned each individual grid slot will check how many bombs are around them
-//          - This is achived using the forEach function
-//          - Checking each slot and whether the each a bomb on the surrounding grid and log the amout
+// !          This may cause me some trouble in figuring out how to check the 8 squares around the slot
+//          - This is achived using the forEach function, checking each slot and whether the each a bomb on the surrounding grid and log the amount
 //          - It will display the about of bombs
 
 // * Once the game has begun the player can choose which grid slot to select
 //   This will begin the timer
-//   Once the slot is select it will either show
-//      - An empty show an empty slow which will begin a flood fill and check all the connecting empty slots
+//   Once the slot is clicked it will either show
+//      - An empty slot which will begin a flood fill and check all the connecting empty slots
 //      - A number of the amout of bombs which are surounding it
 //      - A bomb which will end the game
 //          - A message will apear and the player will be able to play again
+//          - A replay button will take the player to the start screen
+
+
 // ? How will the player put flag on the slots they think have bombs inside
 //   The player will right click the slot and a class will be added which displays a image of a flag to notify the player.
 
 // * The player will keep on going until he has completed the board and what is left are the slots which have the bombs inside
 //      - A message will appear congratulating them on winning the game
-//      - The time take will appear underneath
+//      - The time taken will appear underneath
 //      - They will click a button which allows them to play again
